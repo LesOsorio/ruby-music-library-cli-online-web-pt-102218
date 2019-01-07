7a243db17db9288941931c19b65528a13c823d37
Leslie Osorio <lesosorio17@gmail.com>
2019-01-07 22:50:49 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -94,9 +94,7 @@ end
     if (1..Song.all.length).include?(input)
       song = list_of_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
-    unless
-      song = not_found
-    end
+    
   end
 end
 
