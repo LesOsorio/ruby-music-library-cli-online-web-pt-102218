0aebea896e1baabf31ee723c510ebd97cfd1b42a
Leslie Osorio <lesosorio17@gmail.com>
2019-01-07 22:49:09 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -93,11 +93,12 @@ end
     input = gets.strip.to_i
     if (1..Song.all.length).include?(input)
       song = list_of_songs[input+2]
-      puts "Playing #{song.name} by #{song.artist.name}" 
-    unless 
+      puts "Playing #{song.name} by #{song.artist.name}"
+    unless
       song == nil
     end
-  end
+  end 
+end
 
 
 end
